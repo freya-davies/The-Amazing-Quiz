b77193b9cdfc362e949a19cfcae946fcdeeab87f
student <student@eda-bc-04.localdomain>
2019-10-25 13:23:43 +1300

    create post for counting win points

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const data = require('./data.json')
-const fs = require('fs')
+const addPoint = require('./game')
+const scoreLog = require('./scores.json')
 
 function getQuiz(data, id) {
     return data.questions.find(questions => questions.id == id)
@@ -13,11 +14,19 @@ router.get('/', (req, res) => {
     res.render(template, viewData)
 })
 
+
+
 router.get('/questions/:id', (req, res) => {
     let id = req.params.id
-    let viewData = getQuiz(data, id-1)
+    let viewData = getQuiz(data, id)
     const template = 'question'
     res.render(template, viewData)
+})
+
+router.post('/questions/:id', (request, response) => {
+    let nextId = Number(request.params.id)+1
+    response.redirect('/quiz/questions/'+ nextId)
+    // addPoint(scoreLog) 
 })
 
 module.exports = router
